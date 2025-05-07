23baa75ca3cc198649dbf15049f76968b1cc4a08
Kawasaki93 <108570453+Kawasaki93@users.noreply.github.com>
2025-05-07 19:48:24 +0200

    Update script.js

--- script.js
+++ script.js
@@ -900,14 +900,3 @@ function descargarLog() {
 
 
 
-if ('serviceWorker' in navigator) {
-  window.addEventListener('load', () => {
-    navigator.serviceWorker.register('/service-worker.js')
-      .then(registration => {
-        console.log('Service Worker registrado con éxito:', registration);
-      })
-      .catch(error => {
-        console.log('Error al registrar el Service Worker:', error);
-      });
-  });
-}
